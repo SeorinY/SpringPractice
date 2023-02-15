4a817976d7f64140fa4e76d73395bd21c94e1300
유서린 <sprout3082@gmail.com>
2023-02-16 02:34:14 +0900

    [practice] : NotFound Exception 추가

--- practice/src/main/java/seorin/org/practice/exception/NotFoundException.java
+++ practice/src/main/java/seorin/org/practice/exception/NotFoundException.java
@@ -1,2 +1,14 @@
-package seorin.org.practice.exception;public class NotFoundException {
+package seorin.org.practice.exception;
+
+import seorin.org.practice.Http.ErrorType;
+
+public class NotFoundException extends BusinessException {
+
+    public NotFoundException(ErrorType errorType) {
+        super(errorType);
+    }
+
+    public NotFoundException() {
+        super(ErrorType.NOT_FOUND_RESOURCE);
+    }
 }
