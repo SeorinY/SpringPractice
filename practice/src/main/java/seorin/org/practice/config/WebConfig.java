123f932ae99cf5bb6243544ab5f2da5e1143ed2d
유서린 <sprout3082@gmail.com>
2023-02-28 02:38:15 +0900

    [practice] : WebConfig 추가

--- practice/src/main/java/seorin/org/practice/config/WebConfig.java
+++ practice/src/main/java/seorin/org/practice/config/WebConfig.java
@@ -1,2 +1,21 @@
-package seorin.org.practice.config;public class WebConfig {
+package seorin.org.practice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import seorin.org.practice.jwt.userId.UserIdArgumentResolver;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final UserIdArgumentResolver userIdArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(userIdArgumentResolver);
+    }
 }
