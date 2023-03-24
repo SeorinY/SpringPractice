8765b1d18edbbc0dc80287eac13a4d1d45eb9431
유서린 <sprout3082@gmail.com>
2023-03-25 02:40:26 +0900

    [practice] : User Login Test Controller

--- practice/src/main/java/seorin/org/practice/jwt/login/dto/UserLoginRequestDto.java
+++ practice/src/main/java/seorin/org/practice/jwt/login/dto/UserLoginRequestDto.java
@@ -1,4 +1,4 @@
-package seorin.org.practice.jwt.login;
+package seorin.org.practice.jwt.login.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -12,14 +12,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserLoginRequestDto {
 
-    @Email(message = "이메일 형식에 맞지 않습니다")
-    @NotBlank
-    private String email;
-
-    @NotNull
-    @Pattern(
-            regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
-            message = "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 20자의 비밀번호여야 합니다"
-    )
-    private String password;
+//    @Email(message = "이메일 형식에 맞지 않습니다")
+//    @NotBlank
+//    private String email;
+//
+//    @NotNull
+//    @Pattern(
+//            regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
+//            message = "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 20자의 비밀번호여야 합니다"
+//    )
+//    private String password;
 }
--- practice/src/main/java/seorin/org/practice/jwt/login/dto/UserLoginResponseDto.java
+++ practice/src/main/java/seorin/org/practice/jwt/login/dto/UserLoginResponseDto.java
@@ -1,2 +1,18 @@
-package seorin.org.practice.jwt.login.dto;public class UserLoginResponseDto {
+package seorin.org.practice.jwt.login.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class UserLoginResponseDto {
+
+    private Long userId;
+    private String accessToken;
+
+    public static UserLoginResponseDto of(Long userId, String accessToken) {
+        return new UserLoginResponseDto(userId, accessToken);
+    }
 }
