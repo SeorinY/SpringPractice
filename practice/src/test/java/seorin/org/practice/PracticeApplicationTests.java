243da42aa253fd28c1f2413cb20c8736d509a82d
유서린 <sprout3082@gmail.com>
2023-05-28 22:39:00 +0900

    [init] - project 생성

--- practice/src/test/java/seorin/org/practice/PracticeApplicationTests.java
+++ practice/src/test/java/seorin/org/practice/PracticeApplicationTests.java
@@ -1,10 +1,10 @@
-package springboot.practice.mysql;
+package seorin.org.practice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MysqlApplicationTests {
+class PracticeApplicationTests {
 
 	@Test
 	void contextLoads() {
